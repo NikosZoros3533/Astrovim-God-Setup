4ccbb490114d5f16afab8429eb0c7deef4125a7c
NikosZoros3533 <nikos7kozani@gmail.com>
2026-05-07 14:06:25 +0300

    gruvbox theming

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,6 +6,11 @@ return {
     },
     filesystem = {
       follow_current_file = true,
-    }
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
   },
 }
