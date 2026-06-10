55bc3eb71b76ed46faa3f0cb8a88656edfa4731a
NikosZoros3533 <nikos7kozani@gmail.com>
2026-06-10 16:54:52 +0300

    keymaps love updated

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,11 +1,13 @@
 local M = {}
 
 local love_job = nil
+local love_term_buf = nil
 
 local function get_root() return vim.fs.root(0, { "main.lua", "conf.lua" }) end
 
 function M.run()
   local root = get_root()
+
   if not root then return vim.notify("No LÖVE root", vim.log.levels.ERROR) end
 
   if love_job then return vim.notify "Already running" end
@@ -18,15 +20,40 @@ function M.run()
   vim.notify "LÖVE started"
 end
 
+function M.runWithLog()
+  local root = get_root()
+
+  if not root then return vim.notify("No LÖVE root", vim.log.levels.ERROR) end
+
+  if love_job then return vim.notify "Already running" end
+
+  local Terminal = require("toggleterm.terminal").Terminal
+
+  local love_term = Terminal:new {
+    cmd = "lovec .",
+    dir = root,
+    direction = "horizontal",
+    size = 12,
+    close_on_exit = false,
+    hidden = true,
+  }
+
+  love_term:toggle()
+
+  love_job = love_term
+
+  vim.notify "LÖVE started with console"
+end
 function M.stop()
   if love_job then
-    vim.fn.jobstop(love_job)
+    love_job:shutdown()
     love_job = nil
     vim.notify "LÖVE stopped"
   end
 end
 
 vim.keymap.set("n", "<leader>vr", M.run, { desc = "Run LÖVE" })
+vim.keymap.set("n", "<leader>vc", M.runWithLog, { desc = "Run LÖVE with console" })
 vim.keymap.set("n", "<leader>ve", M.stop, { desc = "Stop LÖVE" })
 
 return M
